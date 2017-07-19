986a574734a65a001572eeaaed65d69bb835a6e9
NojanPasha <nojan@recruit.ai>
2017-07-19 01:27:58 -0700

    Update three.py

--- three.py
+++ three.py
@@ -3,11 +3,10 @@ from networkx.generators.atlas import *
 from networkx.algorithms.isomorphism.isomorph import graph_could_be_isomorphic as isomorphic
 import random
 
-def atlas6():
-    """ Return the atlas of all connected graphs of 6 nodes or less.
-        Attempt to check for isomorphisms and remove.
-    """
+def
 
+.remove_node(n)
+        U = nx.disjoi
     Atlas = graph_atlas_g()[0:208] # 208
       C = nx.connected_component_subgraphs(U)
     # remove isolated nodes, only connected graphs are left
